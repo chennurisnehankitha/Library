897ea825f929c5df477a8879c0051dc8a6ea933c
Snehankitha Ch <chennurisnehankitha@gmail.com>
2026-02-26 12:13:27 -0600

    modified the application

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('learning-experience');
+  protected readonly title = signal('library');
 }
